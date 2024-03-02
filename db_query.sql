f84aaf3e83faa99ac4e56e16dc41dc7ab00959d6
AntonSaikia <antonsaikia11@gmail.com>
2024-03-02 20:17:12 +0100

    Updated

---
+++ db_query.sql
@@ -0,0 +1,10 @@
+-- Uncomment and use the desired sql query template
+
+-- DROP TABLE table_name;
+
+-- CREATE TABLE table_name (item_type TEXT NOT NULL PRIMARY KEY, Tier NOT NULL);
+
+-- INSERT INTO table_name (item_type, Tier)
+-- VALUES ("item_name", "item_tier");
+
+-- SELECT * FROM animes;
